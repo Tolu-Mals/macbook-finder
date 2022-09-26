028f929dc5750121140107087621064f4d1f24ec
Tolu-Mals <malomo.tolz@gmail.com>
2022-09-26 17:26:19 +0100

    Extend default theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,29 @@
 import { Routes, Route } from 'react-router-dom';
 import {
   ChakraProvider,
-  theme,
+  extendTheme,
 } from "@chakra-ui/react";
 import Home from './pages/Home';
 import Details from './pages/Details';
+import '@fontsource/poppins/800.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/inter';
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      100: '#92D2FD',
+      200: '#A6D9FC',
+      300: '#56BAFE',
+      400: '#4693C7',
+      500: '#2687C9',
+    },
+  },
+  fonts: {
+    heading: `'Poppins', sans-serif`,
+    base: `'Inter', sans-serif`
+  }
+});
 
 export const App = () => (
   <ChakraProvider theme={theme}>
