ca612fa0830ea6e9cd2f9a92a44eef93478f08fd
Tolu-Mals <malomo.tolz@gmail.com>
2022-10-10 13:06:04 +0100

    Make pop up show once
    Before now, the "Inspired by my obssession with macbooks" pop up shows up every single time
    we go to the home page and that's not good. It only shows up once

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,18 +4,23 @@ import { Divider, useToast, Box } from '@chakra-ui/react';
 import { useEffect } from 'react';
 import Reviews from '../components/Reviews/Reviews';
 
+let isOnBoarded = false;
+
 const Home = () => {
   const toast = useToast();
 
   useEffect(() => {
     const toastTimeoutId = setTimeout(() => {
-      toast({
-        title: 'Inspired by my obsession with Macbooks 😂',
-        description: "Let's find one for you!",
-        status: 'info',
-        duration: 6000,
-        isClosable: true,
-      });
+      if(!isOnBoarded){
+        toast({
+          title: 'Inspired by my obsession with Macbooks 😂',
+          description: "Let's find one for you!",
+          status: 'info',
+          duration: 6000,
+          isClosable: true,
+        });
+        isOnBoarded = true;
+      }
     }, 2000);
 
     return () => {
